204c38186096e62e3e1231b1187d2406a4ec6062
danson <dansonkalaghe@gmail.com>
2022-04-08 22:35:59 +0000

    fix error

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -28,7 +29,7 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
 }
 
@@ -39,6 +40,5 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
-
 
 export default listOfStudents;
